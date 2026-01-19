3a1f7f69a9377dc7cdeefa49dad9453987b339fa
Igor Zemskov <igor@zemskoff.su>
2026-01-19 23:46:05 +0300

    feat: initialize FastAPI app and integrate categories router

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.routers import categories
+
+app = FastAPI(
+    title="FastAPI Интернет-магазин",
+    version="0.1.0",
+)
+
+app.include_router(categories.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Добро пожаловать в API интернет-магазина!"}
