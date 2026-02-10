3db31d166b17ecaa63ea8940989b2aabe27bdd1d
Igor Zemskov <igor@zemskoff.su>
2026-02-10 18:46:09 +0300

    feat(models): add Review model with relationships to Product and User

---
+++ app/models/reviews.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from sqlalchemy import Text, Boolean, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.database import Base
+
+class Review(Base):
+    __tablename__ = "reviews"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    comment: Mapped[str | None] = mapped_column(Text, nullable=True)
+    comment_date: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now)
+    grade: Mapped[int] = mapped_column(Integer, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
+
+    buyer: Mapped["User"] = relationship("User", back_populates="reviews")
+    product: Mapped["Product"] = relationship("Product", back_populates="reviews")
+    
