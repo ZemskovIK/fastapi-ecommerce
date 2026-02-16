4719ceec7167bfe03729afc9727a4edf634c43e6
Igor Zemskov <igor@zemskoff.su>
2026-02-16 10:40:47 +0300

    feat(schemas): update ProductCreate to support form data and remove image_url

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel, Field, ConfigDict, EmailStr
+from fastapi import Form
+from typing import Annotated
 from decimal import Decimal
 from datetime import datetime
 
@@ -36,9 +38,25 @@ class ProductCreate(BaseModel):
     description: str | None = Field(None, max_length=500,
                                        description="Описание товара (до 500 символов)")
     price: Decimal = Field(..., gt=0, description="Цена товара (больше 0)", decimal_places=2)
-    image_url: str | None = Field(None, max_length=200, description="URL изображения товара")
     stock: int = Field(..., ge=0, description="Количество товара на складе (0 или больше)")
     category_id: int = Field(..., description="ID категории, к которой относится товар")
+
+    @classmethod
+    def as_form(
+            cls,
+            name: Annotated[str, Form(...)],
+            price: Annotated[Decimal, Form(...)],
+            stock: Annotated[int, Form(...)],
+            category_id: Annotated[int, Form(...)],
+            description: Annotated[str | None, Form()] = None,
+    ) -> "ProductCreate":
+        return cls(
+            name=name,
+            description=description,
+            price=price,
+            stock=stock,
+            category_id=category_id,
+        )
 
 
 class Product(BaseModel):
