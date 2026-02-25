f778a6f26cb9d7bf3d088c3a09502387e2079962
Igor Zemskov <igor@zemskoff.su>
2026-02-25 09:42:53 +0300

    chore(logging): add request logging middleware with loguru and unique log_id

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from uuid import uuid4
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
+from loguru import logger
 
 from app.routers import categories, products, users, reviews, cart, orders, payments
 
@@ -8,6 +11,8 @@ app = FastAPI(
     title="FastAPI Интернет-магазин",
     version="0.1.0",
 )
+
+logger.add("info.log", format="Log: [{extra[log_id]}:{time} - {level} - {message}]", level="INFO", enqueue = True)
 
 app.mount("/media", StaticFiles(directory="media"), name="media")
 
@@ -18,6 +23,23 @@ app.include_router(reviews.router)
 app.include_router(cart.router)
 app.include_router(orders.router)
 app.include_router(payments.router)
+
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
+    
 
 @app.get("/")
 async def root():
